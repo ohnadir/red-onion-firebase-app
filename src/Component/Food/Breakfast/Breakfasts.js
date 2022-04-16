c908069b8663838407b6e4a6acc0dd533030f012
ohnadir <qx.nadir@gmail.com>
2022-04-16 10:59:17 +0600

    createFakeDataBase

--- src/Component/Food/Breakfast/Breakfasts.js
+++ src/Component/Food/Breakfast/Breakfasts.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import breakfast1 from '../../../images/breakfast/breakfast1.png'
-import breakfast2 from '../../../images/breakfast/breakfast2.png'
-import breakfast3 from '../../../images/breakfast/breakfast3.png'
-import breakfast4 from '../../../images/breakfast/breakfast4.png'
-import breakfast5 from '../../../images/breakfast/breakfast5.png'
-import breakfast6 from '../../../images/breakfast/breakfast6.png'
+import useFoods from '../../Share/useFoods';
 import Breakfast from './Breakfast';
 
-const breakfasts = [
-    {id:201, name:"Bagel and cream cheese", price:6.99, blogs:"How we dream about our future", img:breakfast1},
-    {id:202, name:"Breakfast sandwich", price:9.99, blogs:"How we dream about our future", img:breakfast2},
-    {id:203, name:"Baked Chicken", price:10.99, blogs:"How we dream about our future", img:breakfast3},
-    {id:204, name:"Eggs Benedict", price:8.99, blogs:"How we dream about our future", img:breakfast4},
-    {id:205, name:"Toast Croissant Fried egg", price:19.99, blogs:"How we dream about our future", img:breakfast5},
-    {id:206, name:"Full Breakfast Fried egg Toast Brunch", price:3.99, blogs:"How we dream about our future", img:breakfast6}
-]
 const Breakfasts = () => {
+    const [foods] = useFoods();
     return (
         <div>
             <div className='lg:max-w-5xl mx-auto gap-7 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 md:max-w-2xl sm:max-w-xl '>
                 {
-                    breakfasts.map(breakfast => <Breakfast
+                    foods.slice(0,6).map(breakfast => <Breakfast
                     key={breakfast.id}
                     breakfast={breakfast}
                     ></Breakfast>)
--- src/Component/Food/Dinners/Dinners.js
+++ src/Component/Food/Dinners/Dinners.js
@@ -1,29 +1,14 @@
 import React from 'react';
-import dinner1 from '../../../images/dinner/dinner1.png' 
-import dinner2 from '../../../images/dinner/dinner2.png'
-import dinner3 from '../../../images/dinner/dinner3.png'
-import dinner4 from '../../../images/dinner/dinner4.png'
-import dinner5 from '../../../images/dinner/dinner5.png'
-import dinner6 from '../../../images/dinner/dinner6.png'
+import useFoods from '../../Share/useFoods';
 import Dinner from './Dinner';
-const dinners = [
-    { id: 101, name: "Salmon with Grapefruit and Lentil Salad", blogs: "How We dream about out future", price: 9.99, img:dinner1 },
-    { id: 102, name: "lemony Salmon Piccata", blogs: "How We dream about out future", price: 10.99, img:dinner2 },
-    { id: 103, name: "Pork Tenderloin With Quinoa Pilaf", blogs: "How We dream about out future", price: 12.99, img:dinner3 },
-    { id: 104, name: "French fries with cheese", blogs: "How We dream about out future", price: 8.99, img:dinner4 },
-    { id: 105, name: "Garlic Butter Baked Salmon", blogs: "How We dream about out future", price: 6.99, img:dinner5 },
-    { id: 106, name: "Baked Chicken", blogs: "How We dream about out future", price: 9.99, img:dinner6 },
-    
-]
-
-
 
 const Dinners = () => {
+    const [foods] = useFoods();
     return (
         <div>
             <div className='lg:max-w-5xl mx-auto gap-7 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 md:max-w-2xl sm:max-w-xl '>
                 {
-                    dinners.map(dinner => <Dinner
+                    foods.slice(12,18).map(dinner => <Dinner
                         key={dinner.id}
                         dinner={dinner}
                     ></Dinner>)
