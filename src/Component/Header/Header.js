7c1be16053488dec45631b9557d0405f7e7d0501
ohnadir <qx.nadir@gmail.com>
2022-04-16 16:56:57 +0600

    add cart update badge

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBars, faTimes, } from "@fortawesome/free-solid-svg-icons"
 import CustomLink from '../CustomLink/CustomLink';
 import { BsCart2 } from 'react-icons/bs';
 import logo from '../../images/logo2.png'
 import { useNavigate } from 'react-router-dom';
+import { CartContext } from '../../App';
 const Header = () => {
     const [open, setOpen] = useState(false);
+    const [cart] = useContext(CartContext);
     const navigate = useNavigate();
     const handleLogo = () => {
         navigate('/home');
@@ -17,7 +19,12 @@ const Header = () => {
                 <img onClick={handleLogo} className='w-24 cursor-pointer' src={logo} alt="" />
             </div>
             <div className="flex gap-4 ml-10 items-center hidden md:flex">
-                <CustomLink to='/cart'><BsCart2/></CustomLink>
+                <CustomLink className='flex relative' to='/cart'><BsCart2 /><span className='
+                text-red-500 text-sm
+                h-4 w-4 text-center
+                flex items-center justify-center
+                rounded-full p-2 absolute right-[-9px] top-[-13px] bg-red-100'>{cart.length}</span>
+                </CustomLink>
                 <CustomLink to='/login'>Login</CustomLink>
                 <CustomLink className='bg-red-500 px-3 rounded-full py-[3px] text-white' to='/signup'>Sign Up</CustomLink>
             </div>
